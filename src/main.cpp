032e05c07799b10d6ff616a5c75526f3f6416a15
willmo3 <thelordofbouge@gmail.com>
2025-10-21 17:34:45 -0400

    Add file I/O sanity test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+#include <fstream>
+#include "cereal/archives/json.hpp"
+#include "Winterval.hpp"
+
 int main() {
+    // Quick I/O sanity test.
+    std::ofstream file("out.json");
+    cereal::JSONOutputArchive archive(file);
+
+    Winterval w = Winterval(-2, 3);
+    w = w * Winterval(4, 5) + Winterval(-1, 1);
+
+    archive(w);
     return 0;
 }
